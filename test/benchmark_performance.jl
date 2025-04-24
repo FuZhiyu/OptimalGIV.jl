d4e652e3756da7eb50195b6c12862619743b5098
Zhiyu Fu <fuzhiyu0@gmail.com>
2025-04-23 19:16:46 -0500

    benchmark performance changes

--- test/benchmark_performance.jl
+++ test/benchmark_performance.jl
@@ -1,7 +1,8 @@
 using Test, GIV, Random
 using DataFrames, CSV
+using BenchmarkTools
 Random.seed!(6)
-simmodel = GIV.SimModel(T = 400, N = 100, varᵤshare = 0.8, usupplyshare = 0.0, h = 0.3, σᵤcurv = 0.2, ζs = 0.0, NC = 2, M = 0.5, σζ = 0.0)
+simmodel = GIV.SimModel(T=400, N=100, varᵤshare=0.8, usupplyshare=0.0, h=0.3, σᵤcurv=0.2, ζs=0.0, NC=2, M=0.5, σζ=0.0)
 df = DataFrame(simmodel.data)
 df.group = mod.(df.id , 10)
 
@@ -10,6 +11,15 @@ id = :id
 t = :t
 weight= :absS
 df = preprocess_dataframe(df, f, :id, :t, :absS; quiet = true)
+q, p, C, Cp, η, S, exclmat, obs_index = GIV.generate_matrices(df, f, id, t, weight)
+
+@btime err1 = GIV.moment_conditions(ones(10), q, Cp, C, S, exclmat, obs_index, Val{:iv}())
+@btime err2 = GIV.moment_conditions(ones(10), q, Cp, C, S, exclmat, obs_index, Val{:iv_legacy}())
+# err2 = GIV.moment_conditions(ones(10), q, Cp, C, S, exclmat, obs_index, Val{:iv_legacy}())
+@profview [GIV.moment_conditions(ones(10), q, Cp, C, S, exclmat, obs_index, Val{:iv_legacy}()) for _ in 1:10]
+# @time GIV.moment_conditions(ones(10), q, Cp, C, S, exclmat, obs_index, Val{:iv}())
+@code_warntype GIV.moment_conditions(ones(10), q, Cp, C, S, exclmat, obs_index, Val{:iv}())
+@code_warntype GIV.calculate_entity_variance(S, obs_index)
 @time givmodel_uu = giv(
     df,
     f,
@@ -17,8 +27,21 @@ df = preprocess_dataframe(df, f, :id, :t, :absS; quiet = true)
     :t,
     :absS;
     guess = Dict("group" => ones(10)),
+    algorithm=:iv_legacy,
+    return_vcov=false,
+    solver_options=(; show_trace=true)
+)
+
+@time givmodel_uu = giv(
+    df,
+    f,
+    :id,
+    :t,
+    :absS;
+    guess=Dict("group" => ones(10)),
     algorithm=:iv,
     return_vcov=false,
+    solver_options=(; show_trace=true)
 )
 
 @time givmodel_uu = giv(
